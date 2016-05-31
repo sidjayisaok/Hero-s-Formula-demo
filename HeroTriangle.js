5d419bcb41fe81995cc0d5f22b912a3ccfbf6565
sidjayisaok <sidjay18@gmail.com>
2016-05-31 01:41:20 -0400

    minor debugging

--- HeroTriangle.js
+++ HeroTriangle.js
@@ -10,11 +10,18 @@ var AreaTriangle = function (a,b,c){
 		var AreaC=parseFloat(c);
 		//write results on HTML
 		{
+			if((AreaA > Math.pow((AreaB*AreaB)+(AreaC*AreaC),.5)) || (AreaB > Math.pow((AreaA*AreaA)+(AreaC*AreaC),.5)) || (AreaC > Math.pow((AreaA*AreaA)+(AreaB*AreaB),.5))) {
+				alert("illegal action performed: Please keep triangle ratios within known models");
+			}
 			//basic formula
 			var Peri = (AreaA + AreaB + AreaC)/2; {
 			var finalAnswer = Math.pow(((Peri)*(Peri-AreaA)*(Peri-AreaB)*(Peri-AreaC)),.5);
 			}
-			document.getElementById("solve").innerHTML = "<h5>Side one is <code>" + (AreaA) + "</code> <p> Side two is <code>" + (AreaB) + "</code></p> <p> Side three is <code>" + (AreaC) + "</code></p></h5> <p> <h4>The area of the triangle is <code>" + (finalAnswer) + "</code></p></h4>";
+
+			document.getElementById("solve").innerHTML = "<h5>Side one is <code>" + (AreaA) 
+			+ "</code> <p> Side two is <code>" + (AreaB) 
+			+ "</code></p> <p> Side three is <code>" + (AreaC) 
+			+ "</code></p></h5> <p> <h4>The area of the triangle is <code>" + (finalAnswer) + "</code></p></h4>";
 			}
 		}
 	}
